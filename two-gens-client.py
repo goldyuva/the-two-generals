9bac2656bd9626cc8efa4f093e9e24792f3983f5
Yuvalg1 <yuvalgoldvarg@gmail.com>
2021-12-25 02:25:25 +0200

    Started from scratch the client and fixed the UDP broadcast. Added threads to the server so that it can send broadcast every second and recieve messages from client

--- two-gens-client.py
+++ two-gens-client.py
@@ -2,25 +2,37 @@ import socket
 
 
 # The IP address of the client
+#hostname = socket.gethostname()
+#host = socket.gethostbyname(hostname)
 host = '127.0.0.1'
 
 # Define the port on which you want to connect
 port = 13117
+addr = (host, port)
 
-serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+udpRecvSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+udpRecvSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    udpRecvSocket.bind(('', port))
+except socket.error as e:
+    print(e)
 
 # connect to server on local computer
-serverSocket.connect((host,port))
+#udpRecvSocket.connect((host,port))
 
 print("Client Started, listening for offer requests...")
-while True:
-
-    # messaga received from server
-    data = serverSocket.recv(1024)
-
-    # print the received message
-    # here it would be a reverse of sent message
-    print('Received offer from {0}, Attempting to connect'.format(str(data.decode('ascii'))))
+# message received from server
+data = udpRecvSocket.recv(1024).decode()
+# print the received message
+print('Received offer from {0}, Attempting to connect'.format(data))
+tcpSendSocket = socket.socket()
+try:
+    tcpSendSocket.connect((data, port))
+except socket.error as e:
+    print(e)
+dataReq = tcpSendSocket.recv(1024).decode()
+name = input(dataReq)
+tcpSendSocket.send(name.encode())
     # Start the game
 # close the connection
-serverSocket.close()
+tcpSendSocket.close()
--- two-gens.py
+++ two-gens.py
@@ -2,52 +2,110 @@ import socket
 import time
 from _thread import *
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = '127.0.0.1'
+port = 13117
+addr = (host, port)
 
-# The IP address of the server
-host = "127.0.0.1"
-#The port number the server broadcasts to
-portNum = 13117
-#The number of active threads that checks if we can start the game
-clientCount = 0
-
-# Try to start server
-try:
-    serverSocket.bind((host, portNum))
-except socket.error as e:
-    print(str(e))
-# Server started
-print("Server started, listening on", serverSocket.getsockname()[0])
-# Listen for connections
-serverSocket.listen(5)
-
-# Define what to do in thread when client is recieved
 def clientThread(connection):
-    print("recieved offer from", connection.getsockname()[0])
-    # Run until we recieve something that is not data
-    while True:
-        # Allocate recieved data
-        connection.send(str(serverSocket.getsockname()[0]).encode('ascii'))
-        buffer = connection.recv(1024)
-        if not buffer:
-            break
+    connection.send(b"Enter team name: ")
+    teamName = connection.recv(1024)
+    print(teamName)
+    connection.wait()
     # Close connection
     connection.close()
+    print("Client connection closed.")
 
-# While running
-while True and clientCount <= 2:
-    # Wait for clients
-    client,address = serverSocket.accept()
-    # Client connected
-    print("connected to {}, {}", address[0], str(address[1]))
-    # Start a new thread for client
-    start_new_thread(clientThread, (client,))
-    # Update the number of clients
-    clientCount += 1
-    print("Thread count =", clientCount)
-    # Wait 1 second
-    time.sleep(1)
-# And now we let the games begin
-time.sleep(10)
-# startGame()
-serverSocket.close()
+def recieve_Thread():
+    clientCount = 0
+    tcpRecvSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        tcpRecvSocket.bind(addr)
+    except socket.error as e:
+        print(e)
+    tcpRecvSocket.listen(5)
+    # While running
+    while True and clientCount <= 2:
+        # Wait for clients
+        client,address = tcpRecvSocket.accept()
+        # Client connected
+        print("connected to {0}, {1}".format(address[0], str(address[1])))
+        # Start a new thread for client
+        start_new_thread(clientThread, (client,))
+        # Update the number of clients
+        clientCount += 1
+        print("Thread count =", clientCount)
+        # Wait 1 second
+    # And now we let the games begin
+    time.sleep(10)
+    # startGame()
+    tcpRecvSocket.close()
+
+def sendUDP():
+    udpSendSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        udpSendSocket.connect((host, port))
+    except socket.error as e:
+        print(e)
+
+    while True:
+        udpSendSocket.sendall(host.encode('ascii'))
+        time.sleep(1)
+
+start_new_thread(sendUDP, ())
+recieve_Thread()
+
+
+
+#   udpSendSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#   udpSendSocket.bind(('', 13117))
+#   # The IP address of the client
+#   #hostname = socket.gethostname()
+#   #host = socket.gethostbyname(hostname)
+#   host = '127.0.0.1'
+#   
+#   #The port number the server broadcasts to
+#   portNum = 13117
+#   udpSendSocket.connect((host, portNum))
+#   #The number of active threads that checks if we can start the game
+#   clientCount = 0
+#   
+#   # Try to start server
+#   try:
+#       udpSendSocket.bind((host, portNum))
+#   except socket.error as e:
+#       print(str(e))
+#   # Server started
+#   print("Server started, listening on", host)
+#   # Listen for connections
+#   
+#   # Define what to do in thread when client is recieved
+#   def clientThread(connection):
+#       print("recieved offer from", connection.getsockname()[0])
+#       # Run until we recieve something that is not data
+#       while True:
+#           # Allocate recieved data
+#           connection.send(str(udpSendSocket.getsockname()[0]).encode('ascii'))
+#           buffer = connection.recv(1024)
+#           if not buffer:
+#               break
+#       # Close connection
+#       connection.close()
+#   
+#   # While running
+#   while True and clientCount <= 2:
+#       # Wait for clients
+#       udpSendSocket.sendall(str(host).encode('ascii'))
+#       time.sleep(1)
+#       #   client,address = udpSendSocket.accept()
+#       #   # Client connected
+#       #   print("connected to {}, {}", address[0], str(address[1]))
+#       #   # Start a new thread for client
+#       #   start_new_thread(clientThread, (client,))
+#       #   # Update the number of clients
+#       #   clientCount += 1
+#       #   print("Thread count =", clientCount)
+#       #   # Wait 1 second
+#   # And now we let the games begin
+#   time.sleep(10)
+#   # startGame()
+#   udpSendSocket.close()
