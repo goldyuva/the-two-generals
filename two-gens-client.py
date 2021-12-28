5fcf87dd6d74effdad4c9491c8bb77328ead80f5
talgoldv <talgoldv@post.bgu.ac.il>
2021-12-28 17:23:20 +0200

    implemented asyncio

--- two-gens-client.py
+++ two-gens-client.py
@@ -7,6 +7,7 @@ import select
 import sys
 import getch
 import random
+import asyncio
 
 # The IP address of the client
 host = ''
@@ -18,6 +19,10 @@ name = "Admiral Greneral Aladdin {0}".format(random.randint(0, 100))
 print(name)
 data = ''
 unpackedBr = ''
+
+async def ainput() -> str:
+    return await asyncio.get_event_loop().run_in_executor(
+            None, lambda: sys.stdin.read(1))
 
 udpRecvSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 udpRecvSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -37,6 +42,7 @@ while invalidPacket:
         data = udpRecvSocket.recvfrom(1024)
         host = data[1][0]
         unpackedBr = struct.unpack(strFormat, data[0])
+        print(unpackedBr[2])
         if unpackedBr[0] == 0xabcddcba:
             if unpackedBr[1] == 0x2:
                 invalidPacket = False
@@ -44,9 +50,10 @@ while invalidPacket:
         pass
 
 port = unpackedBr[2]
+print(port)
 # print the received message
 print('Received offer from {0}, Attempting to connect'.format(host))
-tcpSendSocket = socket.socket()
+tcpSendSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     tcpSendSocket.connect((host, port))
 except socket.error as e:
@@ -63,26 +70,36 @@ print(welcomeMessage)
 if welcomeMessage.endswith('disconnected.'):
     quit()
 
-def kbhit():
-    ready_input, _, _ = select.select([sys.stdin], [], [], 0)
-    return sys.stdin in ready_input
-
 equation = tcpSendSocket.recv(1024).decode()
 print("How much is {0}? ".format(equation))
 summary = None
+async def keyboard_input():
+    global summary
+    ansFlag = False
+    while not ansFlag and not summary:
+        sleep = False
+        if sleep:
+            await asyncio.sleep(1)
+            print("sleeping works fine")
+        else:
+            ch = await ainput()
+            print("RECEIVED CHAR: {0}".format(ch))
+            if ansFlag == False:
+                tcpSendSocket.send(ch.encode())
+            ansFlag = True
+            print(ch)
 
-ready_sockets, _, _ = select.select([tcpSendSocket], [], [], 1)
-while summary == None:
-    if kbhit():
-        ch = getch.getch()
-        print("RECEIVED CHAR: {0}".format(ch))
-        tcpSendSocket.send(ch.encode())
-        ansFlag = True
-    elif tcpSendSocket in ready_sockets:
-        summary = tcpSendSocket.recv(1024).decode()
+async def receive_message():
+    global summary
+    while summary == None:
+        summary = await asyncio.get_event_loop().run_in_executor(None, lambda: tcpSendSocket.recv(1024).decode())
         print(summary)
-        ansFlag = True
-    ready_sockets, _, _ = select.select([tcpSendSocket], [], [], 1)
+try:
+    asyncio.get_event_loop().run_until_complete(asyncio.wait(
+        [keyboard_input(), receive_message()]
+    ))
+except KeyboardInterrupt:
+    quit()
 # Start the game
 # close the connection
 #tcpSendSocket.close()
